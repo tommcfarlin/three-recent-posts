19a4631fab831964caa5b8626e888e8ed2647828
Tom McFarlin <tom@tommcfarlin.com>
2017-05-25 13:18:17 -0400

    Updating the version number of the plugin.

--- three-recent-posts.php
+++ three-recent-posts.php
@@ -11,7 +11,7 @@
  * Plugin Name: Three Recent Posts
  * Plugin URI:  https://tommcfarlin.com/three-recent-posts/
  * Description: Displays the three mot recent posts in your post editor screen.
- * Version:     0.2.0
+ * Version:     0.3.0
  * Author:      Tom McFarlin
  * Author URI:  https://tommcfarlin.com
  * Text Domain: three-recent-posts
@@ -19,17 +19,20 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-include 'class-meta-box.php';
-include 'class-meta-box-display.php';
-include 'class-post-messenger.php';
-include 'class-post-query.php';
+namespace McFarlin\TRP;
+use McFarlin\TRP\Display\Meta_Box;
 
-add_action( 'add_meta_boxes', 'trp_start' );
+include 'Display/class-meta-box.php';
+include 'Display/class-meta-box-display.php';
+include 'Utility/class-post-messenger.php';
+include 'Utility/class-post-query.php';
+
+add_action( 'add_meta_boxes', __NAMESPACE__ . '\\trp_start' );
 /**
  * Starts the plugin.
  */
 function trp_start() {
 
-	$meta_box = new Meta_Box();
+	$meta_box = new Meta_Box( dirname( __FILE__ ) );
 	$meta_box->init();
 }
