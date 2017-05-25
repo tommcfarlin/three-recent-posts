3d620d7eb04670a3e4c0fe4031dc3ff0fbbb9df2
Tom McFarlin <tom@tommcfarlin.com>
2017-05-25 13:17:59 -0400

    Moving Post Messenger & Query to Utility directory

---
+++ Utility/class-post-query.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Queries the database for three most recent posts.
+ *
+ * @author Tom McFarlin
+ * @since  0.2.0
+ */
+
+namespace McFarlin\TRP\Utility;
+
+/**
+ * Queries the database for three most recent posts. Returns the query to the
+ * caller so that it can be interrogates for posts or not.
+ *
+ * @author Tom McFarlin
+ * @since  0.2.0
+ */
+class Post_Query {
+
+	/**
+	 * A reference to the WP_Query this class wraps.
+	 *
+	 * @access private
+	 * @var    WP_Query
+	 */
+	private $query;
+
+	/**
+	 * Instantiates the class by preparing instance data and executing the
+	 * query so the display can render the contents.
+	 */
+	public function __construct() {
+
+		$this->query = null;
+		$this->get_posts();
+	}
+
+	/**
+	 * Executes the query for returning the post recent posts ordered by date.
+	 *
+	 * @access private
+	 */
+	private function get_posts() {
+
+		$args = array(
+			'post_type'   	 => 'post',
+			'post_status' 	 => 'publish',
+			'posts_per_page' => 3,
+			'orderby'     	 => 'date',
+			'order'       	 => 'desc',
+		);
+		$this->query = new \WP_Query( $args );
+
+		return $this->query;
+	}
+
+	/**
+	 * A helper function to determine if the query has any posts.
+	 */
+	public function has_posts() {
+		return $this->query->have_posts();
+	}
+
+	/**
+	 * A helper function for retrieving the next post in the list of
+	 * posts
+	 */
+	public function the_post() {
+		return $this->query->the_post();
+	}
+}
