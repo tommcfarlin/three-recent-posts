c3b37f1b326ed4f3b696ca90aa284228147ab772
Tom McFarlin <tom@tommcfarlin.com>
2017-05-26 11:19:30 -0400

    Removes manual includes; adds autoloader

--- three-recent-posts.php
+++ three-recent-posts.php
@@ -11,21 +11,19 @@
  * Plugin Name: Three Recent Posts
  * Plugin URI:  https://tommcfarlin.com/three-recent-posts/
  * Description: Displays the three mot recent posts in your post editor screen.
- * Version:     0.3.0
+ * Version:     1.0.0
  * Author:      Tom McFarlin
  * Author URI:  https://tommcfarlin.com
  * Text Domain: three-recent-posts
- * License:     MIT
+ * License:     GPL
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
 namespace McFarlin\TRP;
 use McFarlin\TRP\Display\Meta_Box;
 
-include 'Display/class-meta-box.php';
-include 'Display/class-meta-box-display.php';
-include 'Utility/class-post-messenger.php';
-include 'Utility/class-post-query.php';
+// Import the autoloader.
+include_once 'Includes/autoload.php';
 
 add_action( 'add_meta_boxes', __NAMESPACE__ . '\\trp_start' );
 /**
